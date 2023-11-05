c653bb96401ce2f3d863936eabb4276c7fb067b7
Osama Jandali <osamajandaly@gmail.com>
2023-11-05 09:15:58 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  }), sitemap()]
 });
